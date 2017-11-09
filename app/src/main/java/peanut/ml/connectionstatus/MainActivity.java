84a8002aa4d91a8257a92353d5496df3fc43efb5
Mazharul <sbsxw82@gmail.com>
2017-11-09 18:57:06 +0600

    Initial commit

---
+++ app/src/main/java/peanut/ml/connectionstatus/MainActivity.java
@@ -0,0 +1,33 @@
+package peanut.ml.connectionstatus;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.design.widget.Snackbar;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import static peanut.ml.connectionstatus.NetworkStateChangeReceiver.IS_NETWORK_AVAILABLE;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        IntentFilter intentFilter = new IntentFilter(NetworkStateChangeReceiver.NETWORK_AVAILABLE_ACTION);
+        LocalBroadcastManager.getInstance(this).registerReceiver(new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                boolean isNetworkAvailable = intent.getBooleanExtra(IS_NETWORK_AVAILABLE, false);
+                String networkStatus = isNetworkAvailable ? "connected" : "disconnected";
+
+                Toast.makeText(context, "Network Status:"+ networkStatus, Toast.LENGTH_SHORT).show();
+            }
+        }, intentFilter);
+    }
+}
